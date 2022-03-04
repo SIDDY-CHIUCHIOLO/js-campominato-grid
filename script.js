a90dfcf0f8c2e423cc6d4126ba6697aec7e20a9e
siddhartha chiuchiolo <siddychiuchiolo7@gmail.com>
2022-03-04 18:00:08 +0100

    inserito contenuto nel documento html

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
-//con difficoltà 1 => tra 1 e 100
-//con difficoltà 2 => tra 1 e 81
-//con difficoltà 3 => tra 1 e 49
-//Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.
-
-// 1) crea griglia 
 
 const grigliaWrapper  = document.querySelector(".my-grid-container");
 console.log(grigliaWrapper);
