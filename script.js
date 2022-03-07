4ffce874e04ebb2031a52b3f89039180cbe23f7e
siddhartha chiuchiolo <siddychiuchiolo7@gmail.com>
2022-03-07 17:42:25 +0100

    eliminato commenti nel documento js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-//const grigliaWrapper  = document.querySelector(".my-grid-container");
-//console.log(grigliaWrapper);
-
-//const creazioneGrigliaQuadrati = () => {
-//    const nuovoDiv = document.createElement("div");
-//    nuovoDiv.classList.add("my-square", "border", "border-1", "border-dark")
-//    return nuovoDiv;
-//}
-
-//for (let i = 0; i < 100; i++ ){
-//    const quadrato = creazioneGrigliaQuadrati();
-//
-//    quadrato.addEventListener("click", function () {
-//        quadrato.classList.toggle("my-color-active")
-//   })
-//
-//    grigliaWrapper.appendChild(quadrato)
-//}
 
 document.getElementById("play").addEventListener("click", function () {
     creaNuovaGrigia()
