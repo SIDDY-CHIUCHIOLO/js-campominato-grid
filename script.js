d1f21bf16f54d8a11d624ceb30eb40bd028b4eb0
siddhartha chiuchiolo <siddychiuchiolo7@gmail.com>
2022-03-07 17:32:04 +0100

    modifiche nel documento js

--- script.js
+++ script.js
@@ -1,19 +1,64 @@
+//const grigliaWrapper  = document.querySelector(".my-grid-container");
+//console.log(grigliaWrapper);
 
-const grigliaWrapper  = document.querySelector(".my-grid-container");
-console.log(grigliaWrapper);
+//const creazioneGrigliaQuadrati = () => {
+//    const nuovoDiv = document.createElement("div");
+//    nuovoDiv.classList.add("my-square", "border", "border-1", "border-dark")
+//    return nuovoDiv;
+//}
 
-const creazioneGrigliaQuadrati = () => {
-    const nuovoDiv = document.createElement("div");
-    nuovoDiv.classList.add("my-square", "border", "border-2", "border-dark")
-    return nuovoDiv;
+//for (let i = 0; i < 100; i++ ){
+//    const quadrato = creazioneGrigliaQuadrati();
+//
+//    quadrato.addEventListener("click", function () {
+//        quadrato.classList.toggle("my-color-active")
+//   })
+//
+//    grigliaWrapper.appendChild(quadrato)
+//}
+
+document.getElementById("play").addEventListener("click", function () {
+    creaNuovaGrigia()
+})
+
+function creaNuovaGrigia(){
+    document.getElementById("grid").innerHTML = "";
+    const livelloDifficolta = parseInt(document.getElementById("livello-difficolta").value);
+
+    let numeroCelleRiga;
+    let numeroCelle;
+
+    switch (livelloDifficolta) {
+        case 1:
+        default:
+            numeroCelle = 100;
+            numeroCelleRiga = 10;
+            break;
+        case 2:
+            numeroCelle = 81;
+            numeroCelleRiga = 9;
+            break;
+        case 3:
+            numeroCelle = 49;
+            numeroCelleRiga = 7;
+            break;
+    }
+
+    for (let i = 1; i <= numeroCelle; i++) {
+        const nuovoSquare = creaSquare(i, numeroCelleRiga);
+        nuovoSquare.addEventListener("click", function () {
+            nuovoSquare.classList.add("my-color-active")
+        })
+        document.getElementById("grid").appendChild(nuovoSquare);
+    }
 }
 
-for (let i = 0; i < 100; i++ ){
-    const quadrato = creazioneGrigliaQuadrati();
-
-    quadrato.addEventListener("click", function () {
-        quadrato.classList.toggle("my-color-active")
-    })
-
-    grigliaWrapper.appendChild(quadrato)
+function creaSquare(numero, numeroCelleRiga) {
+    let nuovoSquare = document.createElement("div");
+    nuovoSquare.classList.add("my-square", "border", "border-1", "border-dark", "d-flex", "justify-content-center", "align-items-center");
+    nuovoSquare.style.width = `calc(100% / ${numeroCelleRiga})`
+    nuovoSquare.style.height = nuovoSquare.style.width;
+    nuovoSquare.innerHTML = `<span>${numero}</span>`;
+    return nuovoSquare;
 }
+
